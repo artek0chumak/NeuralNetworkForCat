ab801e0c10a8aaba03597bb52e7c368bea961e24
artek0chumak <artek.chumak@gmail.com>
2017-11-02 00:45:25 +0300

    I can teach it! No errors!

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import core
 
 X, Y = core.load_train_set()
 
-print(core.nn_model(X, Y, layers_dims=[3072, 300, 100, 50, 20, 7, 1], print_cost=True, learning_rate=0.000000002))
+print(core.nn_model(X, Y, layers_dims=[3072, 300, 100, 50, 20, 10, 1], print_cost=True, learning_rate=0.000000002))
